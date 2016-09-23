5dff86f57bc262ab591bb0419d5b0529617b8686
astriddg <astriddegaste@gmail.com>
2016-09-23 13:06:57 +0100

    Coffee wake up
    Website made a few months ago for my local coffee shop.

---
+++ index.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Coffee Wake Cup</title>
+	<meta name="description" content="The best coffee place in Clapham">
+
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
+	
+	<link href="bootstrap.css" rel="stylesheet">
+	<link href="cwc.css" rel="stylesheet">
+
+</head>
+
+<body data-spy="scroll" data-target=".navbar" id="page-top">
+
+
+
+	<!-- header -->
+
+	
+
+	<header class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span> 
+					<span class="icon-bar"></span> 
+				</button> 
+
+				<a class="navbar-brand" href="#page-top"><img id="logo" src="photos/logo.png" alt=""></a>
+			</div>
+			<div class="collapse navbar-collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="hidden"><a href="#page-top"></a></li>
+					<li><a href="#page-top">Home</a></li>
+					<li><a href="#menu">Our Menu</a></li>
+					<li><a href="#news">Our News</a></li>
+					<li><a href="#carousel">Our Gallery</a></li>
+					<li><a href="#contact">Contact Us</a></li>
+				</ul>
+			</div> 
+		</div> 
+	</header>
+
+
+
+	<!-- Slider section -->
+
+	<section class="slider container-fluid">
+
+		
+		<img class="mySlides" src="photos/slides1.jpg" alt="">
+		<img class="mySlides" src="photos/slides2.jpg" alt="">
+		<img class="mySlides" src="photos/slides3.jpg" alt="">
+		<img class="mySlides" src="photos/slides3.jpg" alt="">
+		<div class="opening-times"> <h3> We are open : </h3> <br/> Monday: 6.30am - 7pm <br/> Tuesday: 6.30am - 7pm <br/> Wednesday: 6.30am - 7pm <br/> Thursday: 6.30am - 7pm <br/> Friday: 6.30am - 7pm <br/> Saturday: 6.30am - 7pm <br/> Sunday: 8.30am - 7pm <br/> 
+		</div>
+	</section>
+
+<!-- Menu section -->
+
+	<section class="info">
+		<div class="container">
+			<article id="menu" class="col-md-8">
+			<h1>Our Menu</h1>
+			<br/>
+
+			<h2>Fresh Juices and Smoothies</h2>
+			<table class="table ">
+				<tr>
+					<td>Orange</td>
+					<td>£ 2.60</td>
+					<td>Banana & Mango</td>
+					<td>£ 3.20</td>
+				</tr>
+				<tr>
+					<td>Carrot</td>
+					<td>£ 2.60</td>
+					<td>Citrus & Ginger</td>
+					<td>£ 3.20</td>
+				</tr>
+				<tr>
+					<td>Apple</td>
+					<td>£ 2.60</td>
+					<td>Green-Anti-Bloat</td>
+					<td>£ 3.20</td>
+				</tr>
+				<tr>
+					<td>Green Juice</td>
+					<td>£ 2.95</td>
+					<td>Berry Energy</td>
+					<td>£ 3.30</td>
+				</tr>
+				<tr>
+					<td>Apello (orange, grapefruit, apple)</td>
+					<td>£ 2.95</td>
+					<td>Coconut, Banana, Choco</td>
+					<td>££ 3.30</td>
+				</tr>
+				<tr>
+					<td>Jungle (banana, orange, ginger)</td>
+					<td>£ 2.95</td>
+					<td>Raspberry-Sunrise</td>
+					<td>£ 3.50</td>
+				</tr>
+				<tr>
+					<td>Sparkling Citrus Spritzer</td>
+					<td>£ 2.95</td>
+					<td>Green-Glowing Skin</td>
+					<td>£ 3.50</td>
+				</tr>
+			</table>
+			<br>
+			<h2>Ice Menu</h2>
+			<table class="table ">
+				<tr>
+					<th>Milk Shakes</th>
+					<th></th>
+					<th>Dessert</th>
+					<th></th>
+				</tr>
+				<tr>
+					<td>Banana</td>
+					<td>£ 2.95</td>
+					<td>Affogato</td>
+					<td>£ 2.70</td>
+				</tr>
+				<tr>
+					<td>Strawberry</td>
+					<td>£ 2.95</td>
+					<td><strong>Frozen Strawberry</strong></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Mango/Summer fruit smoothie</td>
+					<td>£ 2.95</td>
+					<td> &nbsp  &nbsp Yoghurt</td>
+					<td>£ 3.20</td>
+				</tr>
+				<tr>
+					<td>Ice Latte/Coffee</td>
+					<td>£ 2.95</td>
+					<td> &nbsp &nbsp Ice-Cream</td>
+					<td>££ 2.80</td>
+				</tr>
+				<tr>
+					<td>Ice Mocha</td>
+					<td>£ 2.95</td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Ice Chocolate</td>
+					<td>£ 2.95</td>
+					<td></td>
+					<td></td>
+				</tr>
+					
+				
+			</table>
+
+
+			</article>
+			<aside class="col-md-4">
+				<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fcoffeewakecup%2Ftimeline&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+			</aside>
+		</div>
+	</section>
+
+<!-- Gallery, news and contact form section -->
+
+<!-- Gallery-->
+
+	<section class="news-and-gallery container-fluid">
+		<div class="row">
+			<div class="col-md-6">
+
+				<div  id="carousel" class="row carousel slide" data-ride="carousel"> 
+					<ol class="carousel-indicators"> 
+						<li data-target="#carousel" data-slide-to="0" class="active"></li> 
+						<li data-target="#carousel" data-slide-to="1"></li> 
+						<li data-target="#carousel" data-slide-to="2"></li> 
+						<li data-target="#carousel" data-slide-to="3"></li> 
+					</ol> 
+
+					<div class="carousel-inner"> 
+						<div class="item active"> <img src="photos/gallery1.jpg" alt="Wake Cup Coffee" alt="" >  </div> 
+						<div class="item"> <img src="photos/gallery2.jpg" alt="" ></div> 
+						<div class="item"> <img src="photos/gallery3.jpg" alt="Wake Cup Coffee" alt="" ></div> 
+						<div class="item"> <img src="photos/gallery4.jpg" alt="Wake Cup Coffee" alt="" ></div> 
+					</div> 
+
+					<a class="left carousel-control" href="#carousel" data-slide="prev"> 
+						<span class="glyphicon glyphicon-chevron-left"></span> 
+					</a> 
+					<a class="right carousel-control" href="#carousel" data-slide="next"> 
+						<span class="glyphicon glyphicon-chevron-right"></span> 
+					</a> 
+
+				</div> 
+
+				<!-- contact form -->
+
+				<div id="contact" class="row contact-form">
+				<h2>Message Us!</h2><br>
+					<?php
+					$action=$_REQUEST['action'];
+					if ($action=="")    /* display the contact form */
+					{
+						?>
+						<form  action="" method="POST" enctype="multipart/form-data" class="">
+							<input type="hidden" name="action" value="submit" >
+							Your name:<br>
+							<input name="name" type="text" value="" size="30" class="form-control"/><br>
+							Your email:<br>
+							<input name="email" type="text" value="" size="30" class="form-control"/><br>
+							Your message:<br>
+							<textarea name="message" rows="7" cols="30" class="form-control"></textarea><br>
+							<input class="btn btn-primary btn-lg btn-block" type="submit" value="Send email"/>
+						</form>
+						<?php
+					} 
+					else                /* send the submitted data */
+					{
+						$name=$_REQUEST['name'];
+						$email=$_REQUEST['email'];
+						$message=$_REQUEST['message'];
+						if (($name=="")||($email=="")||($message==""))
+						{
+							echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+						}
+						else{        
+							$from="From: $name<$email>\r\nReturn-path: $email";
+							$subject="Message sent using your contact form";
+							mail("coffeewakecup@hotmail.co.uk", $subject, $message, $from);
+							echo "Email sent!";
+						}
+					}  
+					?>
+
+				</div>
+			</div>
+
+	<!-- news-->
+
+
+			<div class="col-md-6 news">
+				<div id="news" class="news row">
+					<!-- Blog Post -->
+					<div id="news-title">
+						<h2> Our Latest News</h2>
+					</div>
+					<div id="newsBody">
+						<div class="bd">
+						<!-- Title -->
+						<h4>Summer's here !</h4>
+
+						<!-- Author -->
+						<p class="lead">
+							by <a href="#">Lupita</a>
+						</p>
+
+						<hr>
+
+						<!-- Date/Time -->
+						<span class="glyphicon glyphicon-time"></span> Posted on the 12th of May 2016
+
+						<br><br/>
+
+
+						<!-- Post Content -->
+
+						<p>We've got our brand new summer menu with smoothies, milk shakes, ice lattes and more. Come check it out and ask anyone here!</p>
+
+						<hr>
+						<!-- Blog Post -->
+
+
+						<!-- Title -->
+						<h4>New Branding</h4>
+
+						<!-- Author -->
+						<p class="lead">
+							by <a href="#">Lupita</a>
+						</p>
+
+						<hr>
+
+						<!-- Date/Time -->
+						<span class="glyphicon glyphicon-time"></span> Posted on the 12th of May 2016
+
+						<br><br/>
+
+
+						<!-- Post Content -->
+
+						<p>Our new brand is out, with a new logo, new colour and a new website! We've also changed our loyalty card with a beautiful new design, hopefully you will like it :) We would like to thank every one who helped with this new branding work, you are welcome here whenever you want.</p>
+
+					</div>
+
+				</div>
+			</div>
+		</div>
+		</div>
+	</section>
+
+	<footer>
+	<p>© 2000 – 2016 Astrid de Gasté</p>
+	</footer>
+
+	<!-- jQuery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<!-- Javascript de Bootstrap -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+	<script>
+		var slideIndex = 0;
+		carousel();
+
+		function carousel() {
+			var i;
+			var x = document.getElementsByClassName("mySlides");
+			for (i = 0; i < x.length; i++) {
+				x[i].style.display = "none"; 
+			}
+			slideIndex++;
+			if (slideIndex > x.length) {slideIndex = 1} 
+				x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+$(function () {
+	$('header a').on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000, function(){
+			window.location.hash = hash;
+		});
+ });
+
+    $('a').click(function() {
+      $('#item').collapse('toggle');
+   
+  });
+
+});
+</script>
+</body>
+</html>
